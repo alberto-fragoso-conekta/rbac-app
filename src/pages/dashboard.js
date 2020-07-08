e9dbc459a4fb6992556efc4080e84472ad5ff10b
Alberto <alberto.fragoso@conekta.com>
2020-07-08 18:39:36 -0500

    Implementing Can component

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -1,5 +1,30 @@
 import React from "react";
+import { Redirect } from "react-router-dom";
 
-const DashboardPage = () => <h2>Dashboard</h2>;
+import { AuthConsumer } from "../authContext";
+import Can from "../component/Can";
+import Logout from "../component/Logout";
+import Profile from "../component/Profile";
+import PostsList from "../component/PostsList";
+
+const DashboardPage = () => (
+  <AuthConsumer>
+    {({ user }) => (
+      <Can
+        role={user.role}
+        perform="dashboard-page:visit"
+        yes={() => (
+          <div>
+            <h1>Dashaboard</h1>
+            <Logout />
+            <Profile />
+            <PostsList />
+          </div>
+        )}
+        no={() => <Redirect to="/" />}
+      />
+    )}
+  </AuthConsumer>
+);
 
 export default DashboardPage;
