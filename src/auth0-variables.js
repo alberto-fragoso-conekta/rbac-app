a5ab5fa4a878fe03ce9b8e480a1acec3450049c6
Alberto <alberto.fragoso@conekta.com>
2020-07-07 19:37:11 -0500

    Auth0 configuration

---
+++ src/auth0-variables.js
@@ -0,0 +1,6 @@
+export const AUTH_CONFIG = {
+  domain: "alberto-fragoso.us.auth0.com",
+  roleUrl: "https://rbac-app/role",
+  clientId: "k0kpyBBkQ8azKXuSDmPitfktOBFv7B5A",
+  callbackUrl: "http://localhost:3000/callback"
+};
